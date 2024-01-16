d7d2363b70630167e9d575db87ce291b45e9e33b
rishidadhich001 <156800173+rishidadhich001@users.noreply.github.com>
2024-01-16 18:22:48 +0530

    Add files via upload

---
+++ biodata 1.c.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{ //bio data 
+	printf("Name:Rishi Dadhich\n");
+	printf("Date of Birth:01-09-2003\n");
+	printf("Place of Birth:Surat\n");
+	printf("Education:B.COM\n");
+	printf("Height:5.10\n");
+	printf("Job Occupation:No\n");
+	printf("Weight:68\n");
+	printf("Father Name:Manoj Kumar Dadhich\n");
+	printf("Father Occupation:Astrology\n");
+	printf("Mother Name:Dimpal Dadhich\n");
+	printf("Hometown:Rajaldesar\n");
+	printf("Adress:B-103 Gokuldham App,parvat patiya,surat\n");
+	printf("Contact Number : 9825198350,8401168350\n");
+	printf("thank you\n");
+}

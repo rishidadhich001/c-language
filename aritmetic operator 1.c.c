6ecde1d049e6e9c2cebc1f28726d459c765b2b40
rishidadhich001 <156800173+rishidadhich001@users.noreply.github.com>
2024-01-16 18:22:27 +0530

    Add files via upload

---
+++ aritmetic operator 1.c.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+main()
+{
+	int x,y,z;
+	x=10;
+	y=20;
+	z=x+y;
+	printf("sum=%d\n",z);
+	
+	x=30;
+	y=20;
+	z=x-y;
+	printf("subtract=%d\n",z);
+	
+	
+	x=30;
+	y=20;
+	z=x*y;
+	printf("multiply=%d\n",z);
+	
+	
+	x=40;
+	y=20;
+	z=x/y;
+	printf("divide=%d\n",z);
+	
+	x=30;
+	y=20;
+	z=x%y;
+	printf("percentage=%d\n",z);
+	
+	
+	
+}

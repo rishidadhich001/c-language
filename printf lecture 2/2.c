be37fc1a91bd249d9e2814442e57e6115f8f4533
rishidadhich001 <156800173+rishidadhich001@users.noreply.github.com>
2024-01-17 18:33:38 +0530

    Add files via upload

---
+++ printf lecture 2/2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+main()
+{
+	printf("- - - - - -\n");
+	printf("|         |\n");
+	printf("|         |\n");
+	printf("|  RISHI  |\n");
+	printf("|         |\n");
+	printf("|         |\n");
+	printf("- - - - - -\n");
+}

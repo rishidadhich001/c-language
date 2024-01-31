439b45f69b242bb5a3a39593792f887e15e6fae6
Rishi Dadhich <156800173+rishidadhich001@users.noreply.github.com>
2024-01-31 18:28:34 +0530

    Add files via upload

---
+++ switch/1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+main()
+
+{
+	int month;
+	printf("enter month :");
+	scanf("%d",&month);
+	
+	switch(month)
+	{
+		case 1:printf("January!\n");break;
+		case 2:printf("February!\n");break;
+		case 3:printf("March!\n");break;
+		case 4:printf("April!\n");break;
+		case 5:printf("May!\n");break;
+	    case 6:printf("June!\n");break;
+		case 7:printf("July!\n");break;
+		case 8:printf("August!\n");break;
+	    case 9:printf("September!\n");break;
+        case 10:printf("October!\n");break;
+		case 11:printf("November!\n");break;
+	    case 12:printf("Desember!\n");break;
+	    default:printf("Enter the number between 1 to 12!");
+		
+	}
+}
---
+++ switch/2.c
@@ -0,0 +1,119 @@
+#include<stdio.h>
+main()
+{
+	char ans;
+	int total=0;
+	printf("\t\t\t\t\t\tWelcome to the multiple choice question!");
+	
+	printf("\n\n1.Name the National fruit of India?");
+	printf("\nA.Mango");
+	printf("\nB.Orange");
+	printf("\nC.Graps");
+	printf("\nD.Banana");
+	
+	printf("\nEnter your choice in capital(A,B,C,D) :");
+	scanf(" %c",&ans);
+	
+	switch(ans)
+	{
+		case'A':printf("Correct Answer!");
+		total++;
+		//Q-2-------------------
+			printf("\n\n2.Name the place known as the Roof of the World?");
+			printf("\nA.China");
+			printf("\nB.Tibet");
+			printf("\nC.India");
+			printf("\nD.Asia");
+			
+			printf("\nEnter your Answer in capital(A,B,C,D) :");
+			scanf(" %c",&ans);
+			
+			switch(ans)
+			{
+				case'A':printf("Incorrect Answer!");break;
+				case'B':printf("Correct Answer!");
+				total++;
+				//Q-3-----------------------------
+				printf("\n\n3. Name the smallest continent?");
+				printf("\nA.China");
+				printf("\nB.Tibet");
+				printf("\nC.India");
+				printf("\nD.Australia");
+			
+			printf("\nEnter your choice in capital(A,B,C,D) :");
+			scanf(" %c",&ans);
+			
+			switch(ans)
+			{
+				    case'A':printf("Incorrect Answer!");break;
+					case'B':printf("Incorrect Answer!");break;
+					case'C':printf("Incorrect Answer!");break;
+					case'D':printf("Correct Answer!");
+					total++;
+					//Q-4-------------------
+					    printf("\n\n.Which continent is known as the ‘Dark’ continent?");
+						printf("\nA.China");
+						printf("\nB.Tibet");
+						printf("\nC.Africa");
+						printf("\nD.Australia");
+			
+						printf("\nEnter your choice in capital(A,B,C,D) :");
+						scanf(" %c",&ans);
+						
+						switch(ans)
+						{
+							case'A':printf("Incorrect Answer!");break;
+							case'B':printf("Incorrect Answer!");break;
+							case'C':printf("Correct Answer!");
+							total++;
+							//Q-5------------------------
+							printf("\n\n5.Which festival is called the festival of colours?");
+							printf("\nA.Diwali");
+							printf("\nB.Holi");
+							printf("\nC.Navaratri");
+							printf("\nD.Ramnavami");
+							
+							printf("\nEnter your choice in capital(A,B,C,D) :");
+						    scanf(" %c",&ans);
+						    
+						    switch(ans)
+						    {
+						    	case'A':printf("Incorrect Answer!");break;
+						    	case'B':printf("Correct Answer!");
+						    	total++;
+								break;
+								case'C':printf("Incorrect Answer!");break;
+								case'D':printf("Incorrect Answer!");break;
+								default:printf("Invalid value");break;
+							}
+							
+							break;
+							case'D':printf("Incorrect Answer!");break;
+							default:printf("Invalid value");break;
+						}
+					
+					break;
+					default:printf("Invalid value");break;
+			}
+				
+				
+				
+				break;
+				case'C':printf("Incorrect Answer!");break;
+				case'D':printf("Incorrect Answer!");break;
+				default:printf("Invalid value");break;
+			}
+		
+		
+		
+		
+		break;
+		case'B':printf("Incorrect Answer!");break;
+		case'C':printf("Incorrect Answer!");break;
+		case'D':printf("Incorrect Answer!");break;
+		default:printf("Invalid value");break;
+		
+	}
+	printf("Total Score=%d/5",total);
+	
+}

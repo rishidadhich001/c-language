b48df71808e92cfbb494600311d42ae1b11ee601
Rishi Dadhich <156800173+rishidadhich001@users.noreply.github.com>
2024-02-04 17:32:07 +0530

    Update 1.c

--- switch/1.c
+++ switch/1.c
@@ -13,14 +13,13 @@ main()
 		case 3:printf("March!\n");break;
 		case 4:printf("April!\n");break;
 		case 5:printf("May!\n");break;
-	    case 6:printf("June!\n");break;
+	     	case 6:printf("June!\n");break;
 		case 7:printf("July!\n");break;
 		case 8:printf("August!\n");break;
-	    case 9:printf("September!\n");break;
-        case 10:printf("October!\n");break;
+	    	case 9:printf("September!\n");break;
+        	case 10:printf("October!\n");break;
 		case 11:printf("November!\n");break;
-	    case 12:printf("Desember!\n");break;
-	    default:printf("Enter the number between 1 to 12!");
-		
+	    	case 12:printf("December!\n");break;
+	    	default:printf("Enter the number between 1 to 12!");
 	}
 }
